b9b4d928fcc39f84090abe4d29b529bd6d453567
SeungJae <owen1743@naver.com>
2026-05-05 22:32:45 +0900

    import 수정2

--- Capstone/src/main/java/com/example/Capstone/controller/ReportController.java
+++ Capstone/src/main/java/com/example/Capstone/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.example.Capstone.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,6 @@ import com.example.Capstone.dto.request.CreateReportRequest;
 import com.example.Capstone.service.ReportService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
